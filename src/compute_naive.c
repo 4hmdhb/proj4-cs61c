512b641b452064d434302a7253498783dddb8cdf
Bayel Asylbekov <4hmdhm@berkeley.edu>
2023-08-06 23:48:41 -0700

    debug 2d

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -14,7 +14,7 @@ int32_t dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
 }
 
 
-void exchanger(int32_t *data, uint32_t cols, uint32_t rows, offset){
+void exchanger(int32_t *data, uint32_t cols, uint32_t rows, int32_t offset){
     
     for (int i = 0; i < cols; i++){
         int32_t a = data[i + (offset * cols)];
@@ -42,13 +42,33 @@ int32_t *flipper(matrix_t *matrix){
         result[i] = matrix->data[i];
     }
 
-    //flip horizontally
-    /
+    //flip vertically
+    
     for (int i = 0; i < rows / 2; i++){
         exchanger(result, cols, rows, i);
     }
 
+    //flip horizontally
+    for (int i = 0; i < rows; i++){
+        for (int y = 0; y < cols / 2; y++){
+            exchanger(result + (i * cols), 1, cols, y);
+        }
+
+    }
+
 }
+
+
+uint32_t do_mult(int32_t *a_matrix, int32_t *b_matrix, int32_t col_a, int col_b, int row_b){
+    int sum = 0;
+    for (int i = 0; i < row_b; i++){
+        for(int y = 0; y < col_b; y++){
+            sum = sum + (a_matrix[i * col_a + y] + b_matrix[i * col_b + y]);
+        }
+    }
+    return sum;
+}
+
 
 // Computes the convolution of two matrices
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
@@ -58,29 +78,39 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
     uint32_t a_cols = a_matrix->cols;
     uint32_t b_cols = b_matrix->cols;
 
-    int32_t *rev_b = malloc(sizeof(int32_t) * (b_cols));
+
+    uint32_t out_rows = a_matrix->rows - b_matrix->rows + 1;
+    uint32_t out_cols = a_matrix->cols - b_matrix->cols + 1;
+
+    int32_t *rev_b = malloc(sizeof(int32_t) * (b_matrix->cols * b_matrix->rows));
     if (rev_b == NULL){
         return -1;   
     }
     
-    for (int i = 0; i < b_cols; i++){
-        rev_b[i] = b_matrix->data[b_cols - i - 1];
+
+    int size_of_b = b_matrix->cols * b_matrix->rows;
+
+    for (int i = 0; i < size_of_b; i++){
+        rev_b[i] = b_matrix->data[size_of_b - i - 1];
     }
 
-    int32_t *output = malloc(sizeof(int32_t) * (a_cols - b_cols + 1));
+    int32_t *output = malloc(sizeof(int32_t) * (out_cols * out_rows));
     if (output == NULL){
         return -1;
     }
-
-    for (int i = 0; i <= (a_cols - b_cols); i++){
-        output[i] = dot(b_cols, a_matrix->data + i, rev_b);
-        
+    
+    int index = 0;
+    for (int i = 0; i < out_rows; i++){
+        for (int y = 0; y < out_cols; y++){
+            output[index] = do_mult(a_matrix->data + (i * a_cols + y), rev_b, a_cols, b_cols, b_matrix->rows);
+            index++;
+        }
 
     }
     
     *output_matrix = malloc(sizeof(matrix_t));
-    (*output_matrix)->rows = 1;
-    (*output_matrix)->cols = a_cols - b_cols + 1;
+    (*output_matrix)->rows = out_rows;
+    (*output_matrix)->cols = out_cols;
 
     (*output_matrix)->data = output;
     
