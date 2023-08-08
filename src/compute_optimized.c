906a066fc52517f434e394cf414bdc4199272dde
Bayel Asylbekov <4hmdhm@berkeley.edu>
2023-08-08 09:06:11 -0700

    trying to see if ex2 passes

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -1,26 +1,28 @@
 #include <omp.h>
 #include <x86intrin.h>
-
 #include "compute.h"
+
+
 
 // Computes the dot product of vec1 and vec2, both of size n
 int32_t dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
-
+    
     // TODO: implement dot product of vec1 and vec2, both of size n
     int32_t ans = 0;
-    __m256i ans_vector = _mm256_setzero_si256();
+    __m256i ans_vector = _mm256_set1_epi32(0);
 
-    for (int i = 0; i < n / 8; i++){
-        __m256i a_vector = _mm256_loadu_si256( (__m256i *) vec1 + (i * 8));
-        __m256i b_vector = _mm256_loadu_si256( (__m256i *) vec2 + (i * 8));
-
-        ans_vector = _mm256_add_epi32(ans_vector, _mm256_mullo_epi32(a_vector, b_vector));
+    for (int i = 0; i < n / 8 * 8; i+=8){
+        __m256i a_vector = _mm256_loadu_si256( (__m256i *) (vec1 + i));
+        __m256i b_vector = _mm256_loadu_si256( (__m256i *) (vec2 + i));
+        
+        __m256i tempor = _mm256_mullo_epi32(a_vector, b_vector);
+        ans_vector = _mm256_add_epi32(ans_vector, tempor);
         
 
     }
     
-    int tmp_arr[8] = {0,0,0,0,0,0,0,0};
-    _mm256_store_si256( (__m256i *) tmp_arr, ans_vector);
+    int32_t tmp_arr[8] = {0,0,0,0,0,0,0,0};
+    _mm256_storeu_si256( (__m256i *) tmp_arr, ans_vector);
 
     for(int i = 0; i<8; i++){
         ans = ans +tmp_arr[i];
@@ -31,21 +33,15 @@ int32_t dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
     }
 
     return ans;
-  return -1;
 }
 
 
 uint32_t do_mult(int32_t *a_matrix, int32_t *b_matrix, int32_t col_a, int col_b, int row_b){
     int sum = 0;
 
-    int size = col_b * row_b;
-
-    #pragma omp parallel for
-    for (int i = 0; i < size; i++){
-        int a_row = i / col_b;
-        int a_col = i % col_b;
-        sum = sum + (a_matrix[a_row * col_a + a_col] * b_matrix[i]);
-        
+    #pragma omp parallel for 
+    for (int i = 0; i < row_b; i++){
+            sum = sum + dot(col_b, a_matrix + (i * col_a), b_matrix + (i * col_b));        
     }
     return sum;
 }
@@ -53,6 +49,35 @@ uint32_t do_mult(int32_t *a_matrix, int32_t *b_matrix, int32_t col_a, int col_b,
 
 void exchanger(int32_t *data, uint32_t cols, uint32_t rows, int32_t offset){
     
+
+
+
+    for (int i = 0; i < cols / 8 * 8; i+=8){
+
+        int32_t *data1 = data + (offset * cols);
+        int32_t *data2 = data + ((rows-offset-1) * cols);
+        
+        __m256i a_vector = _mm256_loadu_si256( (__m256i *) (data1 + i));
+        __m256i b_vector = _mm256_loadu_si256( (__m256i *) (data2 + i));
+
+        _mm256_storeu_si256( (__m256i *) (data1 + i), b_vector);
+        _mm256_storeu_si256( (__m256i *) (data2 + i), a_vector);
+
+
+    }
+
+    for (int i = cols/8 * 8; i < cols; i++){
+        
+        int32_t a = data[i + (offset * cols)];
+        int32_t b = data[i + ((rows - offset - 1) * cols)];
+
+        data[i + (offset * cols)] = b;
+        data[i + ((rows - offset - 1) * cols)] = a;
+
+
+    }
+
+/*
     
     for (int i = 0; i < cols; i++){
         int32_t a = data[i + (offset * cols)];
@@ -62,6 +87,7 @@ void exchanger(int32_t *data, uint32_t cols, uint32_t rows, int32_t offset){
         data[i + ((rows - offset - 1) * cols)] = a;
     }
 
+    */
 }
 
 
@@ -72,7 +98,7 @@ void flipper(matrix_t *matrix){
 
 
     //flip vertically
-    
+    #pragma omp parallel for 
     for (int i = 0; i < rows / 2; i++){
         exchanger(matrix->data, cols, rows, i);
     }
@@ -97,8 +123,6 @@ void flipper(matrix_t *matrix){
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
   // output_matrix
-    uint32_t a_cols = a_matrix->cols;
-    uint32_t b_cols = b_matrix->cols;
 
 
     uint32_t out_rows = a_matrix->rows - b_matrix->rows + 1;
